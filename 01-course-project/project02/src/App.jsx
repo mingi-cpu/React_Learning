9b24c6287238215c479e8ab4b7c8138b48453f99
mingi-cpu <a01062474215@gmail.com>
2026-03-24 20:36:08 +0900

    feat:useState를useReducer로 대채하여 사용

--- 01-course-project/project02/src/App.jsx
+++ 01-course-project/project02/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState,useRef } from 'react'
+import { useRef,useReducer  } from 'react'
 import Editor from './components/Editor'
 import Header from './components/Header'
 import List from './components/List'
@@ -21,28 +21,40 @@ import List from './components/List'
       id:2
     }
   ]
+const reducer = (state,action) => {
+   switch(action.type){
+      case "CREATE": return [action.date,...state]
+      case "UPDATE": return state.map((item)=>(item.id===action.targetId? {...item,isDone:!item.isDone}
+      :item))
+      case "DELETE": return state.filter((item)=>item.id!==action.targetId  )
+      default:return state
+   }
+}
 function App() {
-  const[todos,setTodos]=useState(mockData)
+  const [todos, dispatch] = useReducer(reducer,mockData)
   const idref=useRef(3)
   const onCreate= (content) => {
-    const newTodo={
-      id:idref.current++,
-      isDone: false,
-      content,
-      date:new Date().getTime()
-    }
-
-    setTodos([newTodo,...todos])
-  } ;
-
+    dispatch({
+      type: "CREATE",
+      date:{
+        id:idref.current ++,
+        isDone:false,
+        content:content,
+        date: new Date().getTime()
+      }
+    })
+  }
   const onUpdate = (targetId) => {
-    setTodos(todos.map((todo)=>
-      todo.id===targetId
-    ?{...todo,isDone:!todo.isDone}
-    :todo
-  ))}
+      dispatch({
+        type:"UPDATE",
+        targetId:targetId
+      })
+  }
   const onDelete=(targetId)=>{
-    setTodos(todos.filter(todo=>todo.id !== targetId))
+    dispatch({
+      type:"DELETE",
+      targetId:targetId,
+    })
   }
   return (  
     <div className='app '>
